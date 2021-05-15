898df8cb0f9d33cfd960348f521f1156e7e82125
Danny Lin <danny@kdrag0n.dev>
2021-05-15 00:19:18 -0700

    xdroid: vendorsetup: Disable ABI checks
    ABI compatibility checks fail for several reasons:
      - The update to Clang 12 causes some changes, but no breakage has been
        observed in practice.
      - Switching to zlib-ng changes some internal structs, but not the public
        API.
    
    We may fix these eventually by updating the ABI specifications, but it's
    likely not worth the effort for us because of how many repos are affected.
    We would need to fork a lot of extra repos (thus increasing maintenance
    overhead) just to update the ABI specs.
    
    For now, just skip the ABI checks to fix build errors.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,5 @@
 export AOSP_REVISION=$(grep "default revision" ".repo/manifests/default.xml" | awk -F '/' '{print $3}' | awk -F '"' '{print $1}')
+export SKIP_ABI_CHECKS=true
 
 # Welcome Msg
 echo "=========================================="
