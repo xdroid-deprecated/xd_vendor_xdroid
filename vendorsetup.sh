337eb16a2d8060b2684caaeea2ad568f342f8960
xyzuan <xyzuannihboss@gmail.com>
2022-06-05 05:11:22 +0000

    xdroid: RELEASE tag

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,4 @@
 export AOSP_REVISION=$(grep "default revision" ".repo/manifests/default.xml" | awk -F '/' '{print $3}' | awk -F '"' '{print $1}')
-export XDROID_BUILD_TYPE=DEV
 
 # Welcome Msg
 echo "=========================================="
