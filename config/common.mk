b7d36207f4d53ccfbc31b3ee3c41c0e2a336246c
xyzuan <xyzuannihboss@gmail.com>
2022-08-16 10:56:10 +0000

    xdroid: Disable not ready GMS & FU Stuff

--- config/common.mk
+++ config/common.mk
@@ -37,11 +37,11 @@ include vendor/xdroid/config/xd_signed.mk
 include vendor/xdroidui/config.mk
 
 # xd. GMS
-include vendor/google/gms/config.mk
-TARGET_SUPPORTS_GOOGLE_RECORDER ?= false
-TARGET_INCLUDE_STOCK_ARCORE ?= false
-TARGET_INCLUDE_LIVE_WALLPAPERS ?= false
-TARGET_SUPPORTS_QUICK_TAP ?= true
+# include vendor/google/gms/config.mk
+# TARGET_SUPPORTS_GOOGLE_RECORDER ?= false
+# TARGET_INCLUDE_STOCK_ARCORE ?= false
+# TARGET_INCLUDE_LIVE_WALLPAPERS ?= false
+# TARGET_SUPPORTS_QUICK_TAP ?= true
 
 # ART
 # Optimize everything for preopt
@@ -70,15 +70,15 @@ PRODUCT_COPY_FILES += \
     vendor/xdroid/config/permissions/backup.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/backup.xml
 
 # Face Unlock
-TARGET_FACE_UNLOCK_SUPPORTED ?= true
-ifeq ($(TARGET_FACE_UNLOCK_SUPPORTED),true)
-PRODUCT_PACKAGES += \
-    FaceUnlockService
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.face_unlock_service.enabled=$(TARGET_FACE_UNLOCK_SUPPORTED)
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
-endif
+# TARGET_FACE_UNLOCK_SUPPORTED ?= true
+# ifeq ($(TARGET_FACE_UNLOCK_SUPPORTED),true)
+# PRODUCT_PACKAGES += \
+#     FaceUnlockService
+# PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+#     ro.face_unlock_service.enabled=$(TARGET_FACE_UNLOCK_SUPPORTED)
+# PRODUCT_COPY_FILES += \
+#     frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
+# endif
 
 # Enable Android Beam on all targets
 PRODUCT_COPY_FILES += \
