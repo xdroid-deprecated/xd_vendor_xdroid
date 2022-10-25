8d5385680612a85f65ba8b069372736e44efa850
jhenrique09 <jhsv09@gmail.com>
2022-10-25 20:50:10 -0300

    xdroid: vendor: Add face unlock
    Change-Id: If0c9d4546211a97854d54bb97b61d8a8cf3f6b7a

--- config/common.mk
+++ config/common.mk
@@ -118,6 +118,17 @@ SYSTEMUI_OPTIMIZE_JAVA := true
 PRODUCT_DEXPREOPT_QUICKEN_APPS += \
     Dialer
 
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED ?= true
+ifeq ($(TARGET_FACE_UNLOCK_SUPPORTED),true)
+PRODUCT_PACKAGES += \
+    FaceUnlockService
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.face_unlock_service.enabled=$(TARGET_FACE_UNLOCK_SUPPORTED)
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
+endif
+
 # Do not preoptimize prebuilts when building GApps
 DONT_DEXPREOPT_PREBUILTS := true
 
