3bb1c58d2ab95970e4b5987cd318ba391830a7b7
Asriadi Rahim <asriadirahim03@gmail.com>
2022-11-20 05:50:18 +0800

    vendor: add back faceunlock

--- config/common.mk
+++ config/common.mk
@@ -70,15 +70,15 @@ PRODUCT_COPY_FILES += \
     vendor/xdroid/config/permissions/backup.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/backup.xml
 
 # Face Unlock
-# TARGET_FACE_UNLOCK_SUPPORTED ?= true
-# ifeq ($(TARGET_FACE_UNLOCK_SUPPORTED),true)
-# PRODUCT_PACKAGES += \
-#     FaceUnlockService
-# PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-#     ro.face_unlock_service.enabled=$(TARGET_FACE_UNLOCK_SUPPORTED)
-# PRODUCT_COPY_FILES += \
-#     frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
-# endif
+TARGET_FACE_UNLOCK_SUPPORTED ?= true
+ifeq ($(TARGET_FACE_UNLOCK_SUPPORTED),true)
+PRODUCT_PACKAGES += \
+    FaceUnlockService
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.face_unlock_service.enabled=$(TARGET_FACE_UNLOCK_SUPPORTED)
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
+endif
 
 # Enable Android Beam on all targets
 PRODUCT_COPY_FILES += \
